22f6e33c47c5cdaa73e86e2a57a84a5f27618238
MorkCarpenter <f2248472180@outlook.com>
2023-05-15 15:46:47 +0800

    getline函数

---
+++ getline函数/fgm_getline.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include"fgm_getline.h"
+int getline(char** str, size_t* str_len, FILE* stream) {
+	char tmp_str[129];
+	while (1) {
+		(*str) = (char*)realloc(*str, sizeof(char) * ((*str_len) + 130));
+		memset(*str + (*str_len), 0, sizeof(char) * 128);
+		fgets(tmp_str, 129, stream);
+		for (int i = 0; i < 128; ++i) {
+			if (tmp_str[i] == '\n' || tmp_str[i] == 0) {
+				(*str)[(*str_len)] = 0;
+				return 0;
+			}
+			(*str)[(*str_len)++] = tmp_str[i];
+		}
+	}
+
+	return -1;
+}
---
+++ getline函数/fgm_getline.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int getline(char** str, size_t* str_len, FILE* stream);
